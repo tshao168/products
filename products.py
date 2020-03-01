a43bcec39d791da559feebc4ff7d98ef86380354
Tommy Shao <Tommy_shao@yahoo.com>
2020-03-01 20:05:33 +0800

    add read file

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
+#讀取檔案
 products = []
+with open ('products.CSV', 'r') as f:
+	for line in f:
+		if '商品,價格' in line :
+			continue # 跳過這一次的迴1圈,進行下一迴
+		name, price = line.strip().split(',') # 先把換行\n符號處理掉再分割
+		products.append ([name, price])
+print (products)
+
+# 使用者輸入
 while True :
 	name = input ('請輸入商品名稱: ')
 	if name == 'q' :
@@ -14,10 +24,11 @@ while True :
 	products.append ([name, price])
 
 # print (products)
+#印出購買記錄
 for p in products :
 	print (p[0], '的價格是', p[1])
 # 寫入檔案 
-with open ('products.csv', 'w', encoding = 'utf-8') as f : # with 的 功能 是在程式走出with block 就會自動關檔
+with open ('products.csv', 'w') as f : # with 的 功能 是在程式走出with block 就會自動關檔
 	f.write ('商品,價格\n')
 	for p in products :
 		f.write (p[0] + ',' + str(p[1]) + '\n') # 利用+號來聯結字串
