a433a06f58ea0bbc4873e629cd2b36891c4a1d64
Tommy Shao <Tommy_shao@yahoo.com>
2020-03-01 19:11:22 +0800

    fixed encoding problem

--- products.py
+++ products.py
@@ -4,6 +4,7 @@ while True :
 	if name == 'q' :
 		break
 	price = input ('請輸入商品價格: ') 
+	price = int (price)
 	# p = []
 	#p.append (name)
 	#p.append (price)
@@ -15,3 +16,9 @@ while True :
 # print (products)
 for p in products :
 	print (p[0], '的價格是', p[1])
+# 寫入檔案 
+with open ('products.csv', 'w', encoding = 'utf-8') as f : # with 的 功能 是在程式走出with block 就會自動關檔
+	f.write ('商品,價格\n')
+	for p in products :
+		f.write (p[0] + ',' + str(p[1]) + '\n') # 利用+號來聯結字串
+
